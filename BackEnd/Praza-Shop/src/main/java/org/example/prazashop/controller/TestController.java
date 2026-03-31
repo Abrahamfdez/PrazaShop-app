062b87a729f5640cc7eec9e4ca35c2ec808aea0e
Abraham fdez <abrahamfdezbande@gmail.com>
2026-03-31 10:39:41 +0200

    add: configuration DockerFile to test Render WEb

---
+++ BackEnd/Praza-Shop/src/main/java/org/example/prazashop/controller/TestController.java
@@ -0,0 +1,16 @@
+package org.example.prazashop.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+
+    @GetMapping("/hola")
+    public String hola() {
+        return "hola, esto es un test";
+    }
+}
+
